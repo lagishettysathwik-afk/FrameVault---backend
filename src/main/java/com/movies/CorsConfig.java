5ab500e3e4b2c76fd75ff485709f574cebbbe9c1
lagishettysathwik-afk <lagishettysathwik@gmail.com>
2026-03-24 10:24:42 +0530

    fixed cors issue

--- src/main/java/com/movies/CorsConfig.java
+++ src/main/java/com/movies/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://framevault-frontend.onrender.com/")
+                .allowedOrigins("https://framevault-frontend.onrender.com") // ❌ removed slash
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
