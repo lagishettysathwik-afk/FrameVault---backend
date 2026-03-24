51a387aec9cb37fa7098aac1937a3b956cefdf85
lagishettysathwik-afk <lagishettysathwik@gmail.com>
2026-03-24 11:39:19 +0530

    added post api

--- src/main/java/com/movies/controller/MovieController.java
+++ src/main/java/com/movies/controller/MovieController.java
@@ -1,49 +1,28 @@
 package com.movies.controller;
 
 import com.movies.model.Movie;
-import com.movies.service.MovieService;
+import com.movies.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
-@CrossOrigin(origins = "https://framevault-frontend.onrender.com")
 public class MovieController {
 
     @Autowired
-    private MovieService movieService;
+    private MovieRepository movieRepository;
 
-    // GET all movies
-    // http://localhost:8080/api/v1/movies
+    // ✅ GET ALL MOVIES
     @GetMapping
-    public ResponseEntity<List<Movie>> getAllMovies() {
-        return new ResponseEntity<>(movieService.getAllMovies(), HttpStatus.OK);
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
     }
 
-    // GET single movie by imdbId
-    // http://localhost:8080/api/v1/movies/{imdbId}
-    @GetMapping("/{imdbId}")
-    public ResponseEntity<Optional<Movie>> getMovieByImdbId(@PathVariable String imdbId) {
-        return new ResponseEntity<>(movieService.getMovieByImdbId(imdbId), HttpStatus.OK);
+    // ✅ ADD THIS (POST API)
+    @PostMapping
+    public Movie createMovie(@RequestBody Movie movie) {
+        return movieRepository.save(movie);
     }
- // ADD these two endpoints to your existing MovieController
-
- // Get movies by language - GET /api/movies/language/Telugu
- @GetMapping("/language/{language}")
- public ResponseEntity<List<Movie>> getMoviesByLanguage(@PathVariable String language) {
-     List<Movie> movies = movieService.getMoviesByLanguage(language);
-     return ResponseEntity.ok(movies);
- }
-
- // Get all available languages - GET /api/movies/languages
- @GetMapping("/languages")
- public ResponseEntity<List<String>> getAllLanguages() {
-     List<String> languages = movieService.getAllLanguages();
-     return ResponseEntity.ok(languages);
- }
 }
